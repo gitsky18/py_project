50f93fead59624f1e2e14e15161f1ab44b956646
Evgeniy Semenov <02logos12@gmail.com>
2023-07-31 04:39:22 +0300

    funcs add and subtraction

--- funcs.py
+++ funcs.py
@@ -6,3 +6,14 @@ a = int(input())
 b = int(input())
 
 print(add_numbers(a, b))
+
+
+
+def subtraction_numbers(a, b):
+    c = a - b
+    return c
+
+a = int(input())
+b = int(input())
+
+print(subtraction_numbers(a, b))
